8ca8ec8a7a924ea473538b5b871cef8e07b5b070
Andre Vieira <andrevieira19992018@gmail.com>
2024-06-25 19:16:16 -0300

    enviando alterações

--- Material aula/logica-js-projeto_inicial/newFile.js
+++ Material aula/logica-js-projeto_inicial/newFile.js
@@ -1,22 +1,15 @@
-alert('Boas vindas ao jogo do número secreto');
-let numeroScreto = 5;
-console.log(numeroScreto);
-let chute;
-let tentativas = 1;
-
 //enquanto
 while (chute != numeroScreto) {
     chute = prompt('Escolha um número entre 1 e 10');
 
     if (chute == numeroScreto) {
-        alert(`Isso aí! você descobriu o número ${numeroScreto} com ${tentativas} tentativas`);
+        alert(`Isso aí! você descobriu o número ${numeroScreto}`);
     } else {
         if (chute > numeroScreto) {
             alert(`O número secreto é menor que ${chute}`);
         } else {
             alert(`O número secreto é maior que ${chute}`);
         }
-        //tentativas = tentativas + 1;
-        tentativas ++;
+
     }
 }
---
+++ Praticas/Prática aula 3/app.js
@@ -0,0 +1,32 @@
+/* let cont = 1;
+
+while (cont <= 10){
+   console.log(cont);
+   cont++;
+} */
+
+
+
+/* let cont = 10;
+
+while (cont >= 0){
+   console.log(cont);
+   cont--;
+}
+ */
+
+
+/* let cont = prompt('Digite um número inteiro: ');
+
+while (cont >= 0){
+   console.log(cont);
+   cont--;
+} */
+
+let contatodor = 0    
+let numero = prompt ('Digite um número inteiro: ');
+
+while (contatodor <= numero){
+   console.log(contatodor);
+   contatodor++;
+}
