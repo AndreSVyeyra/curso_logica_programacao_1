0d0bb3d40444c03992d44e984c9efa654781770a
Andre Vieira <andrevieira19992018@gmail.com>
2024-06-24 21:07:08 -0300

    inserindo tag
    colocando o tag else quando o numeroSecreto não for verdadeiro

--- Material aula/logica-js-projeto_inicial/app.js
+++ Material aula/logica-js-projeto_inicial/app.js
@@ -1,7 +1,10 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroScreto = 5;
+console.log(numeroScreto);
 let chute = prompt ('Escolha um número entre 1 e 10');
 
 if (chute == numeroScreto ){
-    console.log('Isso aí! você descobriu o número secreto (5)')
+    alert('Isso aí! você descobriu o número secreto (5)')
+} else {
+    alert('Você errou :(')
 }
