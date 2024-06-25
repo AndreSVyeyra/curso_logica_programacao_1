8653334ab241d6be343c4ce315f32af97f1376cd
Andre Vieira <andrevieira19992018@gmail.com>
2024-06-24 21:53:28 -0300

    atualizando código

---
+++ Praticas/Prática aula 2/app.js
@@ -0,0 +1,37 @@
+/* let diaSemana = prompt('Digite o dia da semana atual:');
+if(diaSemana == 'Sabado'|| diaSemana == 'Domingo' || diaSemana == 'sabado' || diaSemana ==  'domingo'|| diaSemana ==  'sábado'){
+   alert('Bom final de semana');
+}else{
+   alert('Boa semana');
+}
+
+ */
+
+/* let numero = parseInt(prompt('Digite o número:'));
+if(numero > 0 ){
+   alert('O número digitado é positivo!');
+}if(numero < 0){
+   alert('O número digitado é negativo');  
+}
+else{
+   alert('Digite um número valido !')
+}
+
+ */
+
+/* let resultadoPontos = parseInt(prompt('Digite o valor do resultado obtidos em pontos:'));
+if (resultadoPontos > 100){
+   alert('Parabéns você venceu!');
+}else{
+   alert('Tente novamente para ganhar! ');
+}
+ */
+
+
+/* let saldo = 10.000
+
+alert(`saldo em conta ${saldo.toFixed(3)} R$`) */
+
+let nome  = prompt('Digite o seu nome: ');
+
+alert(`Seja bem vindo ${nome} ╰(*°▽°*)╯ `);
